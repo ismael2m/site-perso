ad0f9733690ad0e3742cab032451cd7cdb0f80b0
ismael2m <ismael.mmadi11@gmail.com>
2020-03-09 11:19:34 +0100

    DOMPurify deleted

--- src/functions/dangerHtml.js
+++ src/functions/dangerHtml.js
@@ -1,7 +1,7 @@
 // == DOM purify: nettoyer le html pour éviter les soucis de failles XSS
-const createDOMPurify = require('dompurify');
+// const createDOMPurify = require('dompurify');
 
-const DOMPurify = createDOMPurify();
+// const DOMPurify = createDOMPurify();
 
 // const dangerHtml = (info) => ({ __html: DOMPurify.sanitize(info) });
 const dangerHtml = (info) => ({ __html: info });
