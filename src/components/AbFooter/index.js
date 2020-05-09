74e725067d986a2493cdb71d50c6c4bbe480e799
ismael2m <ismael.mmadi11@gmail.com>
2020-05-09 12:56:54 +0200

    refonte profil

--- src/components/AbFooter/index.js
+++ src/components/AbFooter/index.js
@@ -16,9 +16,9 @@ const AbFooter = () => {
         {`${date} Ismael Mmadi`}
       </span>
       <div className="abfooter-icons">
-        <a href="https://www.linkedin.com/in/ismael-mmadi-dev"><FaLinkedinIn className="abfooter-icons-icon footer-in" /></a>
-        <a href="https://www.github.com/ismael2m"><FaGithub className="abfooter-icons-icon footer-git" /></a>
-        <a href="https://www.instagram.com/jesuistongraphiste"><FaInstagram className="abfooter-icons-icon footer-insta" /></a>
+        <a href="https://www.linkedin.com/in/ismael-mmadi-dev"><FaLinkedinIn className="abfooter-icons-icon abfooter-in" /></a>
+        <a href="https://www.github.com/ismael2m"><FaGithub className="abfooter-icons-icon abfooter-git" /></a>
+        <a href="https://www.instagram.com/jesuistongraphiste"><FaInstagram className="abfooter-icons-icon abfooter-insta" /></a>
       </div>
     </div>
   );
