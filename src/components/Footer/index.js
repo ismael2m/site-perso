675158b1cf66b7f6909b48e5a9e96533d5f2d142
ismael2m <ismael.mmadi11@gmail.com>
2020-05-09 10:24:56 +0200

    footer refonte

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // = Icônes
 import { FaInstagram, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { GrMailOption } from 'react-icons/gr';
 import gatsby from '../../../static/icon/gatsby.png';
 import netlify from '../../../static/icon/netlify.png';
 
@@ -18,18 +19,35 @@ const Footer = ({ data } ) => {
 
   return (
     <footer className="footer">
-      <h2 className="footer-question">{question}</h2>
-      <p className="footer-contact">{contact}</p>
-      <a href={`mailto:${email}`}><button type="button" className="footer-button">M'envoyer un mail</button></a>
       <div className="footer-icons">
-        <a href="https://www.linkedin.com/in/ismael-mmadi-dev" target="_blank" rel="noopener noreferrer"><FaLinkedinIn className="footer-icons-icon footer-in" /></a>
+        <div className="footer-icons-box">
+          <a href={`mailto:${email}`}>
+            <GrMailOption className="footer-icons-box-icon" />
+          </a>
+        </div>
+        <div className="footer-icons-box">
+          <a href={`mailto:${email}`}>
+            <FaLinkedinIn className="footer-icons-box-icon" />
+          </a>
+        </div>
+        <div className="footer-icons-box">
+          <a href={`mailto:${email}`}>
+            <FaGithub className="footer-icons-box-icon" />
+          </a>
+        </div>
+        <div className="footer-icons-box">
+          <a href={`mailto:${email}`}>
+            <FaInstagram className="footer-icons-box-icon" />
+          </a>
+        </div>
+        {/* <a href="https://www.linkedin.com/in/ismael-mmadi-dev" target="_blank" rel="noopener noreferrer"><FaLinkedinIn className="footer-icons-icon footer-in" /></a>
         <a href="https://www.github.com/ismael2m" target="_blank" rel="noopener noreferrer"><FaGithub className="footer-icons-icon footer-git" /></a>
-        <a href="https://www.instagram.com/jesuistongraphiste" target="_blank" rel="noopener noreferrer"><FaInstagram className="footer-icons-icon footer-insta" /></a>
+        <a href="https://www.instagram.com/jesuistongraphiste" target="_blank" rel="noopener noreferrer"><FaInstagram className="footer-icons-icon footer-insta" /></a> */}
       </div>
       <div className="footer-copyright">
         <span>
           &copy;
-          {`${date} | ${copyright}`}
+          {`${date}  ${copyright}`}
         </span>
 
         <div className="footer-copyright-tools">
