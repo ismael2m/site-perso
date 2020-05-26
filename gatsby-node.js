c89eea8698eaaa6e93c91a1db5dc81cafd5b6848
ismael2m <ismael.mmadi11@gmail.com>
2020-05-26 09:51:23 +0200

    hot-reload ok

--- gatsby-node.js
+++ gatsby-node.js
@@ -43,3 +43,14 @@ module.exports.createPages = async ({ graphql, actions }) => {
     });
   });
 };
+
+// == Solution React-hot-reload
+module.exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig();
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom',
+    };
+  }
+};
