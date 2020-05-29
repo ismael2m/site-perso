dc562e82217a38d49f9f2a7d360b706443cfbae3
ismael2m <ismael.mmadi11@gmail.com>
2020-05-29 09:24:56 +0200

    supression de meta component

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
-// == Composants
-import Meta from './Meta';
-
 const Head = ({ title }) => {
   switch (title) {
     case '/':
       return (
         <HelmetProvider>
           <Helmet>
-            <Meta />
+            <html lang="fr" />
+            <meta charSet="utf-8" />
+            <meta name="description" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:site_name" content="ismael2m" />
+            <meta property="og:title" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:image" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="905" />
+            <meta property="og:image:height" content="684" />
+            <meta property="og:image:secure_url" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta
+              property="og:description"
+              content="Portfolio D'Ismaël Mmadi, Développeur Web Front-end | React JS"
+            />
+            <meta property="og:url" content="https://www.ismael2m.fr" />
+            <meta property="og:type" content="website" />
             <title>
               Ismaël Mmadi | I2M
             </title>
@@ -25,7 +37,22 @@ const Head = ({ title }) => {
       return (
         <HelmetProvider>
           <Helmet>
-            <Meta />
+            <html lang="fr" />
+            <meta charSet="utf-8" />
+            <meta name="description" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:site_name" content="ismael2m" />
+            <meta property="og:title" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:image" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="905" />
+            <meta property="og:image:height" content="684" />
+            <meta property="og:image:secure_url" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta
+              property="og:description"
+              content="Portfolio D'Ismaël Mmadi, Développeur Web Front-end | React JS"
+            />
+            <meta property="og:url" content="https://www.ismael2m.fr" />
+            <meta property="og:type" content="website" />
             <title>
               IMM |
               {' '}
@@ -39,7 +66,22 @@ const Head = ({ title }) => {
       return (
         <HelmetProvider>
           <Helmet>
-            <Meta />
+            <html lang="fr" />
+            <meta charSet="utf-8" />
+            <meta name="description" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:site_name" content="ismael2m" />
+            <meta property="og:title" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:image" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="905" />
+            <meta property="og:image:height" content="684" />
+            <meta property="og:image:secure_url" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta
+              property="og:description"
+              content="Portfolio D'Ismaël Mmadi, Développeur Web Front-end | React JS"
+            />
+            <meta property="og:url" content="https://www.ismael2m.fr" />
+            <meta property="og:type" content="website" />
             <title>
               {title}
             </title>
@@ -54,7 +96,22 @@ const Head = ({ title }) => {
       return (
         <HelmetProvider>
           <Helmet>
-            <Meta />
+            <html lang="fr" />
+            <meta charSet="utf-8" />
+            <meta name="description" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:site_name" content="ismael2m" />
+            <meta property="og:title" content="Ismaël Mmadi - Portfolio" />
+            <meta property="og:image" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="905" />
+            <meta property="og:image:height" content="684" />
+            <meta property="og:image:secure_url" content="https://i.imgur.com/hF04rz8.jpg" />
+            <meta
+              property="og:description"
+              content="Portfolio D'Ismaël Mmadi, Développeur Web Front-end | React JS"
+            />
+            <meta property="og:url" content="https://www.ismael2m.fr" />
+            <meta property="og:type" content="website" />
             <title>
               Projets |
               {' '}
